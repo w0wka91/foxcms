2c84c8dde5443d5d49cd0e113e4165a137a975f8
Waldemar <waldemar.penner91@gmail.com>
2019-12-09 19:38:36 +0100

    resolve project using the generated name

--- src/main/kotlin/com/wprdev/foxcms/api/ProjectQueryResolver.kt
+++ src/main/kotlin/com/wprdev/foxcms/api/ProjectQueryResolver.kt
@@ -1,7 +1,6 @@
 package com.wprdev.foxcms.api
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver
-import com.wprdev.foxcms.domain.project.domain.Project
 import com.wprdev.foxcms.domain.project.infrastructure.ProjectRepository
 import org.springframework.stereotype.Component
 
@@ -10,16 +9,6 @@ import org.springframework.stereotype.Component
 class ProjectQueryResolver(
         private val projectRepo: ProjectRepository
 ) : GraphQLQueryResolver {
-
     fun projects() = projectRepo.findAll()
-
-    fun project(id: Long?): Project? {
-        return if (id !== null) {
-            val b = projectRepo.findById(id)
-            if (b.isPresent) b.get() else null
-        } else {
-            projectRepo.findAll().first()
-        }
-    }
-
+    fun project(generatedName: String) = projectRepo.findByGeneratedName(generatedName)
 }
--- src/main/kotlin/com/wprdev/foxcms/domain/project/infrastructure/ProjectRepository.kt
+++ src/main/kotlin/com/wprdev/foxcms/domain/project/infrastructure/ProjectRepository.kt
@@ -4,5 +4,5 @@ import com.wprdev.foxcms.domain.project.domain.Project
 import org.springframework.data.repository.CrudRepository
 
 interface ProjectRepository : CrudRepository<Project, Long> {
-
+    fun findByGeneratedName(generatedName: String): Project?
 }
